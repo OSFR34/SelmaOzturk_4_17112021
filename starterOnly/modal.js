d386fea3eb779f4738d1487aa3282aeb18d7beaa
OSFR34 <selma-ozturk@outlook.com>
2022-02-11 12:25:12 +0100

    changements class + explicites

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -39,26 +39,26 @@ const email = document.getElementById('email');
 const birthdate = document.getElementById('birthdate');
 const tournamentQuantity = document.getElementById('quantity');
 const locationSelector = document.getElementsByName("location");
-const errorCityText = document.getElementById('errorCity');
-const checkboxConditions = document.getElementById('checkbox1');
+const errorCityMessage = document.getElementById('errorCity');
+const checkboxConditions = document.getElementById('checkboxCondition1');
 const errorConditions = document.getElementById('errorConditionsText');
 
 //REGEX
-// permets l'acceptation des caractères spéciaux même autre que celle de l'aphabet français
+// permets l'acceptation des caractères spéciaux même autre que celle de l'aphabet français et vérifie si il y a bien 2 caractères minimum.
 let nameRegex = /^[a-zA-ZÀ-ÖØ-öø-ÿ-]{2,}/;
 
 // permets de vérifier une syntaxe est bien de type exemple@email.com
 let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; 
 
 //si la valeur de l'input est invalide: affiche le message d'erreur  et l'encadré rouge ds l'input concerné
-function showError(input) {
+function displayError(input) {
     input.style.border = "solid 2px red";
-    input.nextElementSibling.style.display = "block";
+    input.nextElementSibling.style.display = "block";   
 }
-// si la valeur de l'input est valide:  Rien ne s'affiche ou retrait des dispositifs d'erreurs.
-function showSuccess(input) {
+// si la valeur de l'input est valide:  Rien ne s'affiche car annulation des dispositifs d'erreurs.
+function displaySuccess(input) {
 	input.style.border = 'none';
-    input.nextElementSibling.style.display = "none";
+    input.nextElementSibling.style.display = "none";    
 }
 // VALIDATIONS INPUTS
 
@@ -67,10 +67,10 @@ function showSuccess(input) {
 function verifFirstName() {
     // Si la valeur du champ prénom est vide ou ne correspond pas au REGEX, on affiche l'erreur.
     if (firstName.value === '' || !firstName.value.match(nameRegex)) {
-        showError(firstName);
+        displayError(firstName);
         return false;
     }else {
-        showSuccess(firstName);
+        displaySuccess(firstName);
         return true;
     }
 }
@@ -79,10 +79,10 @@ function verifFirstName() {
 function verifLastName() {
     // Si la valeur du champ nom est vide ou ne correspond pas au REGEX, on affiche l'erreur.
     if (lastName.value === '' || !lastName.value.match(nameRegex)) {
-        showError(lastName);
+        displayError(lastName);
         return false;
     }else {
-        showSuccess(lastName);
+        displaySuccess(lastName);
         return true;
     }
 }
@@ -91,10 +91,10 @@ function verifLastName() {
 function verifEmail() {
     // Si la valeur du champ email correspond au REGEX, le champ est valide. Sinon on affiche l'affichage d'erreur.
     if (email.value.match(emailRegex)) { // si l'email est valide, match le Regex, 
-        showSuccess(email);
+        displaySuccess(email);
         return true;
     }else {
-        showError(email);
+        displayError(email);
         return false;
     }
 }
@@ -103,38 +103,38 @@ function verifEmail() {
 function verifBirthdate() {
     // Si la date de naissance entrée ne se situe pas entre le min et le max défit dans HTML, on affiche l'erreur.
     if (birthdate.value< birthdate.min || birthdate.value > birthdate.max) { //si la date de naissance ne se trouve pas entre le min et le max définit dans le HTML
-        showError(birthdate);
+        displayError(birthdate);
         return false;
     }else {
-        showSuccess(birthdate);
+        displaySuccess(birthdate);
         return true;
     }
 }
 
 function verifTournements() {
-    // Si le nombre entré ne se situe pas entre le min (0) et le max (99), on affiche l'erreur.
+    // Si la valeur est nul ou si la valeur ne se situe pas entre le min (0) et le max (99), on affiche l'erreur.
     if (tournamentQuantity.value === "" || tournamentQuantity.value < 0 || tournamentQuantity.value >99) {
-        showError(tournamentQuantity);
+        displayError(tournamentQuantity);
         return false;
     } else {
-        showSuccess(tournamentQuantity);
+        displaySuccess(tournamentQuantity);
         return true;
     }
 }
 
 function verifCity() {
-    let check1 = false;
-    // On observe les inputs pour savoir s'il y en a un de coché.
+    let checkCity = false;
+    // Mise en place d'une boucle .
     for(i=0; i<locationSelector.length; i++) {
         if (locationSelector[i].checked) {
-           check1 = true; // si l'une est coché alors check1 = true
+           checkCity = true; // si l'une est coché alors checkCity = true
         }
     }
-    if (check1 == false) { // si check1 est = false (càd aucune checkbox n'est checked) alors le message d'erreur s'affiche.
-         errorCityText.style.display = "block";
+    if (checkCity == false) { // si checkCity est = faux (cad aucune checkbox n'est selectionné) alors le message d'erreur s'affiche.
+         errorCityMessage.style.display = "block";
          return false;
     }else {
-         errorCityText.style.display = "none";
+         errorCityMessage.style.display = "none";
          return true;
         }
 }
@@ -161,7 +161,7 @@ form.addEventListener('submit', (e) => {
 
         const modalBody = document.querySelector('.modal-body');
         const successMessage  = document.createElement('button');
-        modalBody.innerHTML = '<h2>Merci pour <br> votre inscription.</h2>';
+        modalBody.innerHTML = '<h1>Merci pour <br> votre inscription</h1>';
         modalBody.style.minHeight = '850px';
         modalBody.style.textAlign = 'center';
         modalBody.style.display = 'flex';
